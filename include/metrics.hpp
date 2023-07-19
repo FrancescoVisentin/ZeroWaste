659cd5d0f8f76d5424b489f9828c1cf73853d24c
Lore <87223829+Lore87@users.noreply.github.com>
2023-07-19 19:54:46 +0200

    Update metrics.hpp

--- include/metrics.hpp
+++ include/metrics.hpp
@@ -5,9 +5,17 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 
 namespace zw {
+
+    struct BoundingBox {
+        int left, top, right, bottom;
+    };
+
+
+
+
     double averagePrecision(const std::vector<std::vector<std::pair<cv::Rect,int>>>& detectedItemsPerTray, std::string resPath);
     double IoU(const std::vector<std::vector<std::pair<cv::Rect,int>>>& detectedItemsPerTray, std::string resPath);
     double leftoverRatio(const std::vector<cv::Mat>& foodMasks, std::string resPath);
