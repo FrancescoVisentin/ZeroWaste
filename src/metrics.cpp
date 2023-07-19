ba0b6e7f0df500a596e9a11e21bed16f1e91802e
Lore <87223829+Lore87@users.noreply.github.com>
2023-07-19 19:31:16 +0200

    Update metrics.cpp

--- src/metrics.cpp
+++ src/metrics.cpp
@@ -6,7 +6,30 @@ using namespace zw;
 
 double zw::averagePrecision(const vector<vector<pair<Rect,int>>>& detectedItemsPerTray, std::string resPath) {
 
-    return 0;
+    vector<int> relevance;
+
+    // Read the IDs from the text file
+    std::ifstream relevantIdsFile(resPath + "/relevant_ids.txt");
+    std::set<int> relevantIds;
+    int id;
+    while (relevantIdsFile >> id) {
+        relevantIds.insert(id);
+    }
+    relevantIdsFile.close();
+    
+    // da  detectedItemsPerTray ricava gli IDs
+    for (const auto& trayItems : detectedItemsPerTray) {
+        for (const auto& item : trayItems) {
+            
+            int relevant = (relevantIds.find(item.second) != relevantIds.end()) ? 1 : 0;
+            relevance.push_back(relevant);
+        }
+    }
+
+    
+    return averagePrecision;
+
+    
 }
 
 double zw::IoU(const vector<vector<pair<Rect,int>>>& detectedItemsPerTray, std::string resPath) {
